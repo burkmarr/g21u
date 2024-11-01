e74e00d396c794afd6873e9c289b8f7c1c7fb4ae
Rich Burkmar <rich.burkmar@gmail.com>
2024-11-01 18:55:17 +0000

    Added file handling option

--- js/options.js
+++ js/options.js
@@ -5,6 +5,7 @@ document.getElementById("filename-format").value = getOpt('filename-format')
 document.getElementById("automatic-playback").checked = getOpt('automatic-playback') === "true"
 document.getElementById("playback-volume").value = getOpt('playback-volume')
 document.getElementById("beep-volume").value = getOpt('beep-volume')
+document.getElementById("file-handling").value = getOpt('file-handling')
 
 export function filenameFormat() {
   setOpt('filename-format', document.getElementById("filename-format").value)
@@ -20,4 +21,8 @@ export function playbackVolume() {
 
 export function beepVolume() {
   setOpt('beep-volume', document.getElementById("beep-volume").value)
+}
+
+export function fileHandling() {
+  setOpt('file-handling', document.getElementById("file-handling").value)
 }
